8cae7a0e841cfe2570dc71afc640b3383d21858b
athiracev <94682674+athiracev@users.noreply.github.com>
2022-01-21 08:37:49 +0530

    Update PGM2.py

--- CO4/PGM2.py
+++ CO4/PGM2.py
@@ -19,5 +19,5 @@ class Bank:
 b=Bank(1001,"aru","savings",3000)
 damount=float(input("enter the amount to be deposited:"))
 print("account balance:",b.deposit(damount))
-wamount=float(input("enter the amount to be withdrawn:"))
+wamount=float(input("enter the amount to withdraw:"))
 print("account balance:",b.withdraw(wamount))
