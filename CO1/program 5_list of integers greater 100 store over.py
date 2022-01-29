3c3bf2b9726584a0264e18397c1c3ec3d81bdbbd
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 13:23:58 +0530

    Update program 5_list of integers greater 100 store over.py

--- CO1/program 5_list of integers greater 100 store over.py
+++ CO1/program 5_list of integers greater 100 store over.py
@@ -1,11 +1,12 @@
 # Prompt the user for a list of integers. For all values greater than 100, store ‘over’ instead
 
-n = int(input("Enter the limit:"))
+n = int(input("Enter the limit :"))
 list = []
 for x in range(n):
-    x = int(input("Enter the numbers:"))
+    x = int(input("Enter the number :"))
     if x < 100:
         list.append(x)
     else:
         list.append("over")
 print(list)
+
