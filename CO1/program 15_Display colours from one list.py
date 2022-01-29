2f3fe0430f8275827d37c18bd4e93bcf9a5866ac
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 20:43:11 +0530

    Update program 15_Display colours from one list.py

--- CO1/program 15_Display colours from one list.py
+++ CO1/program 15_Display colours from one list.py
@@ -1,5 +1,5 @@
 # printout all colors from color list 1 not contained in color list 2
 
-color_list_1=["rose","black","green","yellow","silver","orange"]
-color_list_2=["blue","white","silver","gold","grey","pink",]
-print([i for i in color_list_1 if i not in color_list_2])
+color1=["yellow","red","green","blue"]
+color2=["green","orange","violet"]
+print([i for i in color1 if i not in color2])
