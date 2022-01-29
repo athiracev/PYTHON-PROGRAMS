5f40e4dc68f15cd0adb0120af9afa015a7c10b09
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 18:57:31 +0530

    Update program 7_list equal or not.py

--- CO1/program 7_list equal or not.py
+++ CO1/program 7_list equal or not.py
@@ -3,19 +3,20 @@
 # (b) whether list sums to same value
 # (c) whether any value occur in both
 
-l1 = [20, 40, 30, 10, 90]
-l2 = [50, 25, 40, 90]
-print("length of list1:",len(l1))
-print("length of list1:",len(l2))
+l1=[20,40,30,10,90,15]
+l2=[50,25,40,90]
+print("length of list1 :",l1)
+print("length of list2 :",l2)
 if len(l1)==len(l2):
-    print("Two lists are same length")
-total = sum(l1)
-print("Sum of the list1:", total)
-total = sum(l2)
-print("sum of the list2:", total)
-if sum(l1) == sum(l2):
-    print("Two lists sum are equal")
-print("Two lists have common values:")
+    print("Two lists are of same length")
+total=sum(l1)
+print("sum of list1 :",total)
+total=sum(l2)
+print("sum of list2 :",total)
+if sum(l1)==sum(l2):
+    print("Two lists sum is equal")
+print("Value occur in both:")
 for i in l1:
     if i in l2:
         print(i)
+
