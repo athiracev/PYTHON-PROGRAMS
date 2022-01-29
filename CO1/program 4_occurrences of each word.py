221aa0930788d543bfb740f694a4e7ccc025b257
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 13:17:49 +0530

    Update program 4_occurrences of each word.py

--- CO1/program 4_occurrences of each word.py
+++ CO1/program 4_occurrences of each word.py
@@ -1,11 +1,14 @@
 #  Count the occurrences of each word in a line of text
 
-s = input("Enter line of text:")
-count = dict()
-words = s.split()
+s=input("Enter line of text :")
+count=dict()
+words=s.split()
 for x in words:
-  if x in count:
-    count[x] += 1
-  else:
-    count[x] = 1
-print(count)
+    if x in count:
+        count[x]+=1
+    else:
+        count[x]=1
+print("occurrences of each word from the given text is :",count)
+
+
+
