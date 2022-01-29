d607b071dadc125bde4ee1abea985d0ee904205f
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 13:11:09 +0530

    Update program 3_List_comprehensions.py

--- CO1/program 3_List_comprehensions.py
+++ CO1/program 3_List_comprehensions.py
@@ -3,23 +3,23 @@
 
 num=[10,0,-2,4,6,-7,8]
 positive=[x for x in num if x > 0]
-print(positive)
+print("positive list of numbers are :",positive)
 
 # (b) Square of N numbers
 
-n=int(input("enter the limit:"))
-square=[x*x for x in range(1,n+1)]
-print(square)
+n=int(input("Enter the limit :"))
+square=[x*x for x in  range (1,n+1)]
+print("square of N numbers are:",square)
 
 # (c) Form a list of vowels selected from a given word
 
 word="PredictableENGLISHMalayalam"
 vowels="AaEeIiOoUu"
 newlist=[x for x in word if x in vowels]
-print(newlist)
+print("list of vowels from given word is :",newlist)
 
 # (d) List ordinal value of each element of a word (Hint: use ord() to get ordinal values)
 
-word="english"
+word="PYTHON"
 list=[ord(x) for x in word]
 print(list)
