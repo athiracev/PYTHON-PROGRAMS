729bab2caec9182cdcc1fd417b9b07ee002f57e6
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-29 19:48:12 +0530

    Update program 9_characters exchange.py

--- CO1/program 9_characters exchange.py
+++ CO1/program 9_characters exchange.py
@@ -1,6 +1,5 @@
 # Create a string from given string where first and last characters exchanged.   [eg: python -> nythop]
 
-str = input("Enter a string:")
-print("Entered string:", str)
-print("After exchange:")
-print(str[-1] + str[1:-1] + str[0])
+str=input("Enter the string :")
+print("Entered string :",str)
+print("After exchange :",str[-1]+str[1:-1]+str[0])
