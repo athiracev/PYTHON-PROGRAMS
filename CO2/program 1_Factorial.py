6c8b9669fa7c10a4ec0a09be87500131aae02e52
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-01-30 08:34:32 +0530

    Update program 1_Factorial.py

--- CO2/program 1_Factorial.py
+++ CO2/program 1_Factorial.py
@@ -5,3 +5,13 @@ num = int(input("Enter the number:"))
 for i in range(1,num+1):
     fact = fact * i
 print("Factorial of ",num,"is:", fact)
+
+####  METHOD 2   #######
+
+fact=1
+i=1
+n=int(input("Enter the number :"))
+while i<=n:
+    fact=fact*i
+    i+=1
+print("factorial is :",fact)
